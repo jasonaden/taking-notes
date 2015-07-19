b49d98f45a2a82d154dbe453bc28ae96590099d3
Jason Aden <jasonaden@gmail.com>
2015-07-19 10:09:43 -0700

    Remove the router.

--- client/components/app.ts
+++ client/components/app.ts
@@ -2,21 +2,14 @@
 
 // Angular 2
 import {Component, View, coreDirectives} from 'angular2/angular2';
-import {RouteConfig, RouterOutlet, RouterLink, routerInjectables} from 'angular2/router';
-
-// We use a folder if we want separate files
-import {Home} from './home/home';
-import {Dashboard} from './dashboard/dashboard';
-import {Todo} from './todo/todo';
 
 // App: Top Level Component
 @Component({
-  viewInjector: [routerInjectables],
   selector: 'app' // without [ ] means we are selecting the tag directly,
 })
 @View({
   // needed in order to tell Angular's compiler what's in the template
-  directives: [RouterOutlet, RouterLink, coreDirectives],
+  directives: [coreDirectives],
   styles: [`
     .title { margin: 0.5rem 0; }
     .main-content { padding: 0.5rem; }
@@ -30,22 +23,17 @@ import {Todo} from './todo/todo';
   <section class="main-content">
     <h1 class="title">{{ title }}</h1>
     <nav class="main-nav">
-      <a [router-link]="['/home']">Home</a>
-      <a [router-link]="['/dashboard']">Dashboard</a>
-      <a [router-link]="['/todo']">Todo</a>
+      <a >Home</a>
+      <a >Dashboard</a>
+      <a >Todo</a>
     </nav>
-    <router-outlet></router-outlet>
   </section>
   `
 })
-@RouteConfig([
-  { path: '/', as: 'home', component: Home },
-  { path: '/dashboard', as: 'dashboard', component: Dashboard },
-  { path: '/todo', as: 'todo', component: Todo }
-])
+
 export class App {
   title: string;
   constructor() {
-    this.title = 'Angular2 Minimalist Starter';
+    this.title = 'Taking Notes App';
   }
 }
